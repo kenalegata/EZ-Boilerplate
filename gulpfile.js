fe7d58cc231c466aabaa4fda492a383b888cbc98
Kenneth Alegata <kalegata@freelancer.com>
2017-03-12 18:33:15 +0800

    Add nunjucks for static templating.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var nunjucksRender = require('gulp-nunjucks-render');
 var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+  .pipe(gulp.dest('build'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('sass:dev', function () {
+  return gulp.src('./app/assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./build/assets/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("./sass/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+gulp.task('sass:production', function () {
+ return gulp.src('./app/assets/sass/**/*.scss')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('./build'));
 });
 
+gulp.task('serve', ['nunjucks', 'sass:dev'], function() {
+
+    browserSync.init({
+        server: "./build"
+    });
+
+    gulp.watch("./app/assets/sass/*.scss", ['sass:dev']);
+    gulp.watch("./app/**/*.nunjucks", ['nunjucks']);  
+});
